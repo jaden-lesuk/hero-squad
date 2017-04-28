82a3a778b7eff2702412ed7f82f09c383b93910f
jaden-lesuk <jadenlesuk12@gmail.com>
2017-04-28 12:52:33 +0300

    Added test to see if new hero instantiates correctly

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -2,5 +2,9 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class HeroTest{
-    
+  @Test
+  public void Hero_instantiatesCorrectly_True(){
+    Hero testHero = new Hero("The Incredible Hulk", "Bruce Banner","Male", 32, "Big, green and angry");
+    assertEquals(true, testHero instanceof Hero);  
+  }
 }
