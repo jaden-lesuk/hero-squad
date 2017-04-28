8f17b24db6c9782be0205b044e4143ff298a1c98
jaden-lesuk <jadenlesuk12@gmail.com>
2017-04-28 14:43:13 +0300

    Added private attributes to the Hero class and methods for other classes to access them

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,6 +1,35 @@
 public class Hero{
+  private String mName;
+  private String mSecretIdentity;
+  private String mSex;
+  private int mAge;
+  private String mPowers;
 
   public Hero(String name, String secretIdentity, String sex, int age, String powers){
-    
+    mName = name;
+    mSecretIdentity = secretIdentity;
+    mSex = sex;
+    mAge = age;
+    mPowers = powers;
+  }
+
+  public String getName(){
+    return mName;
+  }
+
+  public String getSecretIdentity(){
+    return mSecretIdentity;
+  }
+
+  public String getSex(){
+    return mSex;
+  }
+
+  public int getAge(){
+    return mAge;
+  }
+
+  public String getPowers(){
+    return mPowers;
   }
 }
