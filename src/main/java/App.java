fb43ee88b86be0f5b42e03e08eeb7111374826d0
jaden-lesuk <jadenlesuk12@gmail.com>
2017-04-28 17:28:24 +0300

    Added port functionality

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -37,5 +37,15 @@ public class App{
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+
+    setPort(port);
+
   }
 }
